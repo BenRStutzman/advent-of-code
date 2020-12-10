eed9f530908d90506cbe73e9b49b29fdd68eff34
BenRStutzman <tysorx89@gmail.com>
2020-12-10 18:06:35 -0500

    Make Dec. 10 better again again

--- 2020/10B.py
+++ 2020/10B.py
@@ -5,8 +5,9 @@ joltages = [0] + sorted([int(num) for num in f.read().splitlines()])
 ways_to_get_tos = [1]
 
 for index, joltage in enumerate(joltages[1:]):
+    real_index = index + 1
     ways_to_get_to = 0
-    for lower_index in range(max(0, index - 2), index + 1):
+    for lower_index in range(max(0, real_index - 3), real_index):
         if joltage - joltages[lower_index] <= 3:
             ways_to_get_to += ways_to_get_tos[lower_index]
     ways_to_get_tos.append(ways_to_get_to)
