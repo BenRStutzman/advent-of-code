c60a967a213769646562758d798fd06eb7092319
BenRStutzman <tysorx89@gmail.com>
2020-12-10 18:04:20 -0500

    Make Dec. 10 better

--- 2020/10B.py
+++ 2020/10B.py
@@ -5,12 +5,11 @@ joltages = [0] + joltages
 
 ways_to_get_tos = [1]
 
-for joltage in joltages[1:]:
+for index, joltage in enumerate(joltages[1:]):
     ways_to_get_to = 0
-    for difference in range(1, 4):
-        lower_joltage = joltage - difference
-        if lower_joltage in joltages:
-            ways_to_get_to += ways_to_get_tos[joltages.index(lower_joltage)]
+    for lower_index in range(max(0, index - 2), index + 1):
+        if joltage - joltages[lower_index] <= 3:
+            ways_to_get_to += ways_to_get_tos[lower_index]
     ways_to_get_tos.append(ways_to_get_to)
 
 print(ways_to_get_tos)
